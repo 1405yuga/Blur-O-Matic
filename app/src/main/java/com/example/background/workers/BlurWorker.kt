5ae9421bf54859996a46afa0e4a35d592241ee8e
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-15 23:07:32 +0530

    re-framing worker and checking inputdata

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -2,9 +2,11 @@ package com.example.background.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.text.TextUtils
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.background.KEY_IMAGE_URI
 import com.example.background.R
 private const val TAG = "Blur Worker"
 
@@ -12,10 +14,20 @@ class BlurWorker(context: Context, parameters: WorkerParameters) : Worker(contex
     override fun doWork(): Result {
 
         val appContext = applicationContext
-        makeStatusNotification("Bluring the image", appContext)
+
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+
         return try {
-            val picture = BitmapFactory
-                .decodeResource(appContext.resources, R.drawable.android_cupcake)
+            makeStatusNotification("Bluring the image", appContext)
+            //val picture = BitmapFactory
+            //    .decodeResource(appContext.resources, R.drawable.android_cupcake)
+
+
+            if(TextUtils.isEmpty(resourceUri)){
+                Log.e(TAG,"invalid input uri")
+                throw IllegalArgumentException("invalid input uri")
+            }
+
             val output = blurBitmap(picture,appContext)
             val outputUri = writeBitmapToFile(appContext,output)
             makeStatusNotification("Output is $outputUri",appContext)
