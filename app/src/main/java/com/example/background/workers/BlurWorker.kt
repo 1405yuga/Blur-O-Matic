19ef1c295315fa6323f74aab646820e809c96c91
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-15 19:41:12 +0530

    created bitmap from cupcake image

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -1,13 +1,19 @@
 package com.example.background.workers
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.background.R
 
-class BlurWorker(context: Context,parameters: WorkerParameters) : Worker(context,parameters) {
+class BlurWorker(context: Context, parameters: WorkerParameters) : Worker(context, parameters) {
     override fun doWork(): Result {
 
         val appContext = applicationContext
-        makeStatusNotification("Bluring the image",appContext)
+        makeStatusNotification("Bluring the image", appContext)
+
+        val picture = BitmapFactory
+            .decodeResource(appContext.resources, R.drawable.android_cupcake)
+
     }
 }
